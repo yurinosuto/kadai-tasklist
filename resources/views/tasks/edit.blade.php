b209e1aaba4a728ceda64a399613eaca3ce40e2c
yurino.suto <ec2-user@ip-172-31-36-30.us-east-2.compute.internal>
2018-06-12 07:14:02 +0000

    CRUD messages

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+ <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+
+    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('更新') !!}
+
+    {!! Form::close() !!}
+
+<!-- ここにページ毎のコンテンツを書く -->
+
+@endsection
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+ <h1>メッセージ一覧</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+ 
+    {!! link_to_route('tasks.create', '新規メッセージの投稿') !!} 
+    
+@endsection
